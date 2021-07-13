d1e5c91fe2e7bfc4e67cbd6c8876c70ccd2a4187
Dinesh <dineshse1986@gmail.com>
2021-07-13 19:30:42 -0400

    Project init

---
+++ chaincode/contracts/InsuranceContract.go
@@ -0,0 +1,23 @@
+package contracts
+
+import (
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+// SimplePaymentContract contract for handling writing and reading from the world state
+type InsuranceContract struct {
+	contractapi.Contract
+}
+
+// Account : User account
+type Account struct {
+	AccountID         string `json:"accountID"`
+	Name              string `json:"name"`
+	Address         bool   `json:"address"`
+}
+
+// Policy : Hold policy data
+type Policy struct {
+	PolicyID string `json:"policyID"`
+}
+
